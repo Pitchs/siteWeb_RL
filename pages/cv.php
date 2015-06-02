93926e3482551df06005c8a1c041907e898968dc
Romain <ledru.romain44@gmail.com>
2015-06-02 17:45:29 +0200

    code page CV

--- pages/cv.php
+++ pages/cv.php
@@ -1,1 +1,32 @@
-<p> cv </p>
+
+<h2>Formation:</h2>
+<p>
+2013-2014 Actuellement en 1ère année de BTS SIO option.</br>
+2012 Bac STI option électronique.</br>
+</p>
+
+<h2>Compétences en informatique :</h2>
+<p>
+Langages C#, Python, PHP, HTML, XMS, CSS, scripts Linux.</br>
+SGBD SQL, Access, MySQL, SQLServer.</br>
+Logiciels MSOffice, OpenOffice, Visio, Gant Project, ….</br>
+Système et réseau Linux, Windows 2008 Server, XP, Seven, …</br>
+Services TCP/IP : adressage IP, DHCP, FTP, DNS.</br>
+Outils de virtualisation : vmware, virtualBox, virtualPC.</br>
+</p>
+<h2>Expériences professionnelles :</h2>
+<p>
+(5 semaines) : Stage de 1ere année BTS(sio) à la référence universitaire de Nantes(travail sur un module de statistiques d’un site web existant).</br>
+(1mois) : Travail à la chaîne et à la logistique dans une cartonnerie (Ondul Pack).</br>
+(4mois) : Vendeur préparateur de commandes dans une rôtisserie sur un marché.</br>
+(5 semaines) : Stage de 1ere année BTS(sio) à la boutique ValletInformatique pour une refonte de leur site web.</br>
+(7 semaines) : Stage de 2°ème année de BTS(sio) au CNAM.</br>
+</p>
+<h2>Connaissances :</h2>
+<p>
+Langues Compréhension globale de l’anglais.</br>
+Manutention Charge et décharge de poids lourds à Ondul Pack à l’aide d’un transpalette manuel. Installer, ranger la camionnette du marché.</br>
+Notions de vendeur  Utilisation d’une caisse enregistreuse, contact avec le client.</br>
+Pratique de sport :</br>
+Arts martiaux Karaté contact (1an), self défense (Aito, 1an).</br>
+</p>
