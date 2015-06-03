e1423b0fc26b185e37144750b63aafec39f7bf82
Romain <ledru.romain44@gmail.com>
2015-06-03 17:53:51 +0200

    pagination

--- pages/textIndex.php
+++ pages/textIndex.php
@@ -1,14 +1,14 @@
 
 <title>Acceuil</title>
 <img src=images/moi.png width=130></img>
-<span>
+<p>
  Bienvenue sur la page de mon portfolio !
 
 Je m’appelle Romain Ledru, je suis actuellement en deuxième année de BTS SIO (Services Informatiques aux Organisations)
- spécialité réseau au lycée <a href="http://carcouet.paysdelaloire.e-lyco.fr/" target=_blank>Carcouët</a>.</br>
+ spécialité réseau au lycée <a href="http://carcouet.paysdelaloire.e-lyco.fr/" target=_blank>Carcouët</a>.</br></br>
 Ce portfolio a été mis en place pour vous présenter un aperçu de mes compétences que j’ai acquises au fil de mon parcours au travers de mes différentes activités.
 Sur cet espace personnalisé vous pourrez retrouver l’intégral des travaux réalisés au cours des années.</br>
 Mes connaissances personnelles seront également présentées afin de vous proposer un éventail plus large de mes connaissances.
 Des liens vers des dossiers téléchargeables seront également mis à disposition.</br>
 Bonne visite !
-</span>
+</p>
