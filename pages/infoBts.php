1e7d6b53240d4e002d05e56b0ac6732d30048143
Romain <ledru.romain44@gmail.com>
2015-06-03 15:57:19 +0200

    code page infoBts

--- pages/infoBts.php
+++ pages/infoBts.php
@@ -1,1 +1,40 @@
-<p>bts Sio</p>
+
+<p>
+Anciennement BTS Informatique de gestion, le BTS Services informatiques aux organisation le remplace depuis la rentré 2011</p>
+<h2>1) Pourquoi un tel changement ?</h2>
+<p>
+Ce nouveau BTS répond d’abord aux attentes de la profession en matière de qualification des techniciens de l'informatique. Il s’inscrit dans une logique d’évolution de la place et du rôle des TIC dans les processus de gestion. Dans cette démarche d'adaptation, le Ministère de l’Éducation nationale a travaillé en coopération étroite avec des professionnels comme IBM et CISCO. Il a également pris en compte la diversité des parcours des étudiants qui s’engagent dans la préparation d’un BTS dans ce secteur : plus de 50% d’entre eux poursuivent leurs études après le BTS.
+</p>
+<h2>2) En quoi le BTS SIO sera-t-il différent du BTS informatique de gestion ?</h2>
+<p>
+Aujourd’hui, les services informatiques cherchent à répondre du mieux possible à leur besoin de performance. Ils participent à la création de valeur. Les questions de disponibilité, de rapidité, de fiabilité et de sécurité de l’information sont stratégiques aussi bien pour les entreprises que pour les administrations. Cette approche "services" demande donc une réponse en termes de "solutions".
+</br>
+Au cours de ses 2 années de formation, l’étudiant aura ainsi le choix entre 2 spécialités :
+</br>
+&nbsp;&nbsp;&nbsp;     SLAM (Solutions Logicielles et Applications Métier - ex Développeur d’applications) : son métier est de concevoir des programmes destinés à la gestion d’une organisation,
+&nbsp;&nbsp;&nbsp;    SISR (Solutions d’infrastructure, systèmes et réseaux - ex Administrateur de réseaux) : son métier est de mettre en place et de gérer des infrastructures réseau au sein d’une organisation. 
+</p>
+<h2>3) Quels seront ses principaux objectifs ?</h2>
+<p>
+    Préparer, dans les meilleures conditions, l'insertion professionnelle des futurs diplômés en privilégiant l'acquisition de bases techniques et la maîtrise de la "relation de services", tournée vers le client.
+    Renforcer l'attractivité du secteur professionnel des technologies de l’information (IT) et des formations qui y conduisent, en particulier auprès d'une cible féminine qui s'en détourne aujourd'hui.
+    Offrir aux futurs diplômés de réelles possibilités de poursuite d'études après le BTS.
+</p>
+<h2>4) Vers quels métiers débouche ce BTS ?</h2>
+<p>
+Le titulaire du BTS SIO travaille, soit en tant que collaborateur d’une organisation, soit en en tant qu'intervenant d'une société d’ingénierie et de services informatiques, d'un éditeur de logiciels, d'une société de conseil en technologies.
+</br>
+Le titulaire du diplôme peut aussi occuper des emplois variés et liés à l’option choisie, tels que : administrateur systèmes et réseaux, informaticien support et déploiement, développeur d'application, intégrateur, etc.
+</p>
+<h2>5) Quelles études permet-il de poursuivre ? Et vers quelles filières ?</h2>
+<p>
+Avec l'aménagement de la formation, un enrichissement des maths, un renforcement de la culture générale et de l'anglais, les opportunités de poursuites d'études sont améliorées. Elles sont fréquentes et appréciées en licence professionnelle car elles apportent à l'étudiant davantage d'expériences terrains. L'objectif affiché est de sécuriser les possibilités de poursuites d'études après le BTS, sans perte de temps. C’est donc un moyen pour les étudiants qui n’ont pas choisi des études longues de continuer et de réussir, jusqu’au niveau master pour les plus motivés.
+</p>
+<h2>6) À quels élèves le BTS SIO s’adresse-t-il ?</h2>
+<p>
+Il s’adresse aux titulaires d’un baccalauréat technologique STG (notamment avec l’option nouvelle Gestion des systèmes d'information - GSI) et STI, d’un baccalauréat général (S, ES), d’un baccalauréat professionnel (SEN).
+</br>
+Cette rénovation est une opportunité remarquable pour attirer des jeunes filles et des jeunes gens dans une filière en plein développement, avec la possibilité d'une professionnalisation rapide dans des métiers très diversifiés. Ce BTS peut aussi constituer une étape, pour les élèves qui en ont la motivation, dans un parcours d’études supérieures qui peut mener jusqu’au master, un diplôme d’ingénieur ou un diplôme d’ESC.
+</br>
+*Approuvé par la commission professionnelle consultative compétente en décembre 2010, le nouveau projet doit être examiné, pour avis, par les autres instances consultatives (CSL, CSE, CNESER) dans le courant du mois de mars.
+</p>
