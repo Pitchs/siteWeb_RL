973e905e20a22e0c72b8401da648bba9b8541e33
Romain <ledru.romain44@gmail.com>
2015-06-08 17:10:45 +0200

    lien de jquery css sur le fichier en nom sur le site

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 				PDF.SetPageMode("none"); //--- cache les signets ---//
 				PDF.setZoom(80%); //--- Zoom le document à 80% ---//
 			</script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="jquery-ui-1.11.4/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
