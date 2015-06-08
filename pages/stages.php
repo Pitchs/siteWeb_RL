221cbe2aeecb6e497126c8297dd9d9180d08e416
Romain <ledru.romain44@gmail.com>
2015-06-08 16:48:37 +0200

    renom travaux en stages

---
+++ pages/stages.php
@@ -0,0 +1,37 @@
+﻿   <p>	Durant ma Formation j'ai dû effectuer deux stages en entreprise.</p>
+		
+<div id="tabs">
+  <ul>
+    <li><a href="#tabs-1">Stage première année</a></li>
+    <li><a href="#tabs-2">Stage deuxième année</a></li>
+  </ul>
+
+  <div id="tabs-1">
+	 <p>
+		Pour ma première année de stage,durant cinqs semaines, j'ai travaillé au magasin Vallet Informatique afin de refaire entièrement leur site web qui avait subi des attaques.
+		Les employés de la boutique n'ayant pas de compétence en développements, il a fallu que je mène le projet seul, et que je leurs fournisse au terme de mon stage des modes opératoires afin de pouvoir modifier, ré-uploader leur site à leurs convenances.
+	</p>
+	<a class="nav" href="http://www.vallet-informatique.com/index.html" target="_blank" >Liens vers le site Vallet Informatique</a></br>
+	</br>
+	<p> Ressources fournis afin de pouvoir modifier le site, après la formation.</p>
+	</br>
+	<object type="application/pdf" name="PDF3" id="PDF" width="100%" height="800px" data="ressources/DocSiteValletInfo2014.pdf">
+		</object> 
+  </div>
+  <div id="tabs-2">
+		<p>
+			Ma deuxième année de stage à eu lieu au CNAM où j'avais pour projet de développer une maquette afin de leur fournir une base de réflexion pour un projet à venir.</br>
+			<h2>Bilan de stage:</h2>
+		</p>
+		
+		<object type="application/pdf" name="PDF" id="PDF" width="100%" height="800px" data="ressources/Compte_rendu_de_stage_cnam.pdf">
+		</object> 
+
+		<p>
+			<h2>Annex de stage :</h2>
+		</p>
+		<object type="application/pdf" name="PDF2" id="PDF" width="100%" height="700px" data="ressources/Annex_compte_rendu_de_stage_cnam.pdf">
+		</object> 
+	</div>
+</div>
+
