306450c7ac27667469d7a75f0a3c8018dcc138b9
Romain <ledru.romain44@gmail.com>
2015-06-08 15:08:36 +0200

    code page contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,1 @@
-
-
-<p>contacte</p>	
+<p>Je reste à votre écoute, pour toute questions et demandes d'informations complémentaires:<A href="ledru.romain44@gmail.com">ledru.romain44@gmail.com</A></p>	
