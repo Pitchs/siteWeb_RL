c8910fdeb2a1fe8ae1af65ec0efefda7a0a52991
Romain <ledru.romain44@gmail.com>
2015-06-08 19:22:04 +0200

    suppr coquille
    suppr coquille

--- pages/travaux.php
+++ pages/travaux.php
@@ -1,1 +1,38 @@
-﻿<p></p>
+﻿<h2 class="docs">Travaux réalisés en PPE.</h2>
+ 
+  <div id="accordion">
+    <h3>IMC 01</h3>
+    <div>
+	<p> Début d'un projet android avec éclipse, prise en main</p>
+		<object type="application/pdf" name="PDF4" id="PDF" width="100%" height="800px" data="ressources/IMC01.pdf">
+		</object> 
+    </div>
+    <h3>IMC 02</h3>
+    <div>
+	<p>Suite du projet android avec éclipse, créer une nouvelle activité</p>
+		<object type="application/pdf" name="PDF4" id="PDF" width="100%" height="800px" data="ressources/IMC02.pdf">
+		</object> 
+	</div>
+    <h3>IMC 04</h3>
+    <div>
+	<p>Suite du projet android avec éclipse, traduire son application en anglais</p>
+     <object type="application/pdf" name="PDF4" id="PDF" width="100%" height="800px" data="ressources/IMC04.pdf">
+		</object> 
+    </div>
+	 <h3>IMC produit</h3>
+    <div>
+	<p>Résultat de mon projet</p>
+
+                <img src="images/formulaire_menu_français.png" width="280px" height="550px">      
+
+               <img border="0" src="images/imh_menu_français_2.png" width="280px" height="550px">    
+
+                <img border="0" src="images/imh_interpreter_français.png" width="280px" height="550px">    
+
+                <img border="0" src="images/imh_menu_anglais.png" width="280px" height="550px">     
+  
+                 <img border="0" src="images/imh_menu_français.png" width="280px" height="550px">    
+  
+ 					
+    </div>
+  </div>
