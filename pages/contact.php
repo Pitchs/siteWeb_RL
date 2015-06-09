39be9f85d4d6be323f0862a13f7ed145c490ebc9
Romain <ledru.romain44@gmail.com>
2015-06-09 11:29:15 +0200

    correct lien mailing

--- pages/contact.php
+++ pages/contact.php
@@ -1,4 +1,4 @@
 <p>Ce site a été versionné avec GitHub, dépôt disponible sur : <a href=" https://github.com/Pitchs/siteWeb_RL" target=_blank> https://github.com/Pitchs/siteWeb_RL</a></p>
 </br>
 <p>Je reste à votre écoute, pour toute questions et demandes d'informations complémentaires, contactez moi à l'adresse suivante :</br>
-<A href="ledru.romain44@gmail.com">ledru.romain44@gmail.com</A></p>	
+<A href="mailto:ledru.romain44@gmail.com">ledru.romain44@gmail.com</A></p>	
